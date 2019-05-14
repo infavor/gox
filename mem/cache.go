543ed868c45ca1f074952bc6798767adc129a386
hehety <hehety@outlook.com>
2019-05-14 22:36:42 +0800

    nothing

---
+++ mem/cache.go
@@ -0,0 +1,9 @@
+package mem
+
+func makeBuffer(size int) []byte {
+	return make([]byte, size)
+}
+
+func Apply(size int) {
+
+}
--- timer/timer_test.go
+++ timer/timer_test.go
@@ -2,19 +2,25 @@ package timer_test
 
 import (
 	"../timer"
+	"fmt"
 	"testing"
+	"time"
 )
 
-func Test1(t *testing.T) {
-	/*timer.Start(3, 1000, 0, func() {
+/*func Test1(t *testing.T) {
+	fmt.Println(time.Now().Second())
+	timer.Start(5000, 1000, 0, func() {
 		time.Sleep(time.Second * 3)
-		fmt.Println("xxx")
+		fmt.Println("xxx", time.Now().Second())
 	})
 	c := make(chan int)
-	<- c*/
+	<- c
+}*/
 
-	timer.Start(3, 0, 1000, func() {
-		panic("xxx")
+func Test2(t *testing.T) {
+	fmt.Println(time.Now().Second())
+	timer.Start(4000, 0, 1000, func() {
+		fmt.Println("xxx", time.Now().Second())
 	})
 	c := make(chan int)
 	<-c
