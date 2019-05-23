17703226283651ea474b83f61e85c73e653522c3
hehety <hehety@outlook.com>
2019-05-23 22:47:34 +0800

    update go mod

--- go.mod
+++ go.mod
@@ -1,8 +1,10 @@
-module gox
+module github.com/hetianyi/gox
+
+go 1.12
 
 require (
 	github.com/json-iterator/go v1.1.6
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.1 // indirect
-	github.com/sirupsen/logrus v1.4.1
+	github.com/sirupsen/logrus v1.4.2
 )
--- httpx/upload.go
+++ httpx/upload.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"errors"
 	"fmt"
+	"github.com/hetianyi/gox/cache"
 	"io"
 	"net/http"
 	"reflect"
@@ -82,14 +83,20 @@ func (reader *FileFormReader) Read(buff []byte) (int, error) {
 
 // beginUpload begin to read request entity and parse form field
 func (handler *FileUploadHandler) Parse() error {
-	handler.formReader = &FileFormReader{
-		request:          handler.Request,
-		unReadableBuffer: new(bytes.Buffer),
-		atomByte:         make([]byte, 1),
-		newLineBytesPair: make([]byte, 2),
-		newLineBuffer:    new(bytes.Buffer),
-		buffer:           make([]byte, 1024*30),
-	}
+	defer func() {
+		cache.ReCacheResource(handler.formReader)
+		handler.formReader = nil
+	}()
+	handler.formReader = cache.ApplyResource(formReaderType, func() interface{} {
+		return &FileFormReader{
+			request:          handler.Request,
+			unReadableBuffer: new(bytes.Buffer),
+			atomByte:         make([]byte, 1),
+			newLineBytesPair: make([]byte, 2),
+			newLineBuffer:    new(bytes.Buffer),
+			buffer:           make([]byte, 1024*30),
+		}
+	}).(*FileFormReader)
 
 	var fileIndex = 0
 
